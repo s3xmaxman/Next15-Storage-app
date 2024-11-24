c4046b7057ce404e7d119262602e705732e0dd61
s3xmaxman <badside3919@gmail.com>
2024-11-24 09:52:54 +0900

    update page.tsx and file.actions.ts

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,11 +1,16 @@
 import Card from "@/components/Card";
 import { getFiles } from "@/lib/actions/file.actions";
+import { getFileTypesParams } from "@/lib/utils";
 import { Models } from "node-appwrite";
 import React from "react";
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
-  const files = await getFiles();
+  const searchText = ((await searchParams)?.search as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
+  const types = getFileTypesParams(type) as FileType[];
+  const files = await getFiles({ types, searchText, sort });
+
   return (
     <div className="page-container">
       <section className="w-full">

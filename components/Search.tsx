a4c26c1a66e6cde3cbecbebac595b0a92ad54323
s3xmaxman <badside3919@gmail.com>
2024-11-24 10:14:32 +0900

    update Search.tsx

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter } from "next/navigation";
+import { sortTypes } from "@/constants";
 
 const Search = () => {
-  return <div>Search</div>;
+  const path = usePathname();
+  const router = useRouter();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sortType) => (
+          <SelectItem
+            key={sortType.label}
+            value={sortType.value}
+            className="shad-select-item"
+          >
+            {sortType.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default Search;
